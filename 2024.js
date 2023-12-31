7d21b7428971baef7f34629e4c510ee39a136a54
TSMJosem <receptorx89@gmail.com>
2023-12-30 20:01:27 -0600

    Updated

---
+++ 2024.js
@@ -0,0 +1,14 @@
+var audio = document.getElementById('Carry-Me-Home');
+var audio1 = document.getElementById('Andar-Conmigo');
+var audio2 = document.getElementById('Limon-Sal');
+var video = document.getElementById('video');
+var contador = document.getElementById('conteo');
+
+document.body.onclick = function() {
+    document.body.onclick = null;
+    video.playbackRate = 0.2;
+    audio.currentTime = 2057;
+    audio1.volume = 0.5;
+    audio.volume = 0.2;
+    audio.play();
+}
